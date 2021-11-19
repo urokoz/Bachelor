8c8825939e9b73425c0184a5b67807d12a10211b
urokoz <urokoz@gmail.com>
2021-11-19 18:00:22 +0100

    canged som shit

--- code/metrics.sh
+++ code/metrics.sh
@@ -2,7 +2,7 @@
 
 WD=$1
 
-./pep_ker.sh ${WD}prepped_data/unfiltered_dataset.csv ${WD}metrics/pep_ker_scores.txt ${WD}peptides/peptides/
+#./pep_ker.sh ${WD}prepped_data/unfiltered_dataset.csv ${WD}metrics/pep_ker_scores.txt ${WD}peptides/peptides/
 
 ./calculate_metrics.py -df ${WD}prepped_data/log_unfiltered_dataset.csv -pf ${WD}peptides/peptides.txt
 ./calculate_metrics.py -df ${WD}prepped_data/log_filtered_dataset.csv -pf ${WD}peptides/peptides.txt
